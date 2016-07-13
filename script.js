d451c3a14ddafa4c2d6f11aefaf10ee52b96a4f7
Mike R <Mike R>
2016-07-12 22:45:17 -0700

    commiting incomplete assignment

---
+++ script.js
@@ -0,0 +1,74 @@
+//STEP 1
+//see index.html
+
+//STEP 2
+//var click = document.getElementById('step2');
+//click.onclick = function () {
+//    window.alert('i have been clicked');
+//};
+
+//STEP 3
+//document.addEventListener("DOMContentLoaded", init, false);
+//function init() {
+//    step2.addEventListener("click", writeMessage, false);
+//}
+//function writeMessage() {
+//    window.alert('i have been clicked');
+//}
+
+//STEP 4
+
+//STEP 5
+
+//STEP 6
+
+//document.addEventListener("DOMContentLoaded", function(){
+//    function stopDefault
+//document.getElementById('redirect').addEventListener('click', stopDefault, false);
+//});
+
+//step 7
+
+//step 8
+
+//step 9
+
+//step 10
+
+//PART 2
+
+//step 1
+window.addEventListener("load", init, false);
+
+//step 2
+function init() {
+//step 3
+    document.getElementById("seven").addEventListener("click", enter);
+    document.getElementById("eight").addEventListener("click", enter);
+    document.getElementById("nine").addEventListener("click", enter);
+    document.getElementById("divide").addEventListener("click", enter);
+    document.getElementById("four").addEventListener("click", enter);
+    document.getElementById("five").addEventListener("click", enter);
+    document.getElementById("six").addEventListener("click", enter);
+    document.getElementById("multiply").addEventListener("click", enter);
+    document.getElementById("one").addEventListener("click", enter);
+    document.getElementById("two").addEventListener("click", enter);
+    document.getElementById("three").addEventListener("click", enter);
+    document.getElementById("subtract").addEventListener("click", enter);
+    document.getElementById("zero").addEventListener("click", enter);
+    document.getElementById("period").addEventListener("click", enter);
+    document.getElementById("add").addEventListener("click", enter);
+    document.getElementById("equal").addEventListener("click", calculate);
+}
+
+//step 4
+function calculate(){
+    
+};
+
+//step 5
+function enter(val) {
+
+}
+
+
